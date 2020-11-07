51d015a34f2c2b667c93f121901da9d5b7101a31
Michael York <18yorkmichael@gmail.com>
2020-11-07 14:01:32 -0600

    Tunnel Fixes
    Some basic fixes for the tunnel scene :)

--- firstHack.c
+++ firstHack.c
@@ -2,9 +2,11 @@
 
 void theTunnels()
 {
+    int playerInjured = 0;
     int answer = 0;
     int answerStatus = 0;
     int figureExamined = 0;
+    char uselessPiece[256];
     printf("\nYou enter the heating tunnels, climbing down a rusty ladder.\n");
     printf("Despite the name, you feel a chill settle deep within your bones.\n");
     while(answerStatus == 0)
@@ -42,15 +44,28 @@ void theTunnels()
         {
             answerStatus = 1;
             figureExamined = 1;
-            printf("You approach the strange figure, and stop a few feet away. It's pretty heavily burned.\n");
-            printf("You flinch backwards as it's head snaps up, and it begins to scream.\nYou stumble to the door, slamming it open. ");
+            answer = 0;
+            printf("You approach the strange figure, and stop a few feet away. It's pretty heavily burned.\n\n");
+            printf("You flinch backwards as it's head snaps up, and it begins to scream.\n You should definitely run [1]!\n > ");
+            scanf("%d", &answer);
+            if (answer == 1)
+            {
+            printf("You scramble to the door, ears ringing as you feel skittering in the ground, coming from behind you.\n");
+            printf("You stumble through the door, slamming it shut behind you.\n");
+            }
+            else
+            {
+                playerInjured = 1;
+                printf("You don't react in time, and get slammed against the concrete. Everything goes black.\n\n\n");
+                printf("You wake up, slumped against the metal door, dried blood staining your hair.\n");
+            }
         }
         else
         {
             printf("Unknown command. Use the bracketed words, and use all caps!");
         }
     }
-    printf("The cold air is a relief, and the heavy metal door blocks the heat from escaping. You slouch against the door\n");
+    printf("The cold air is a relief, and the heavy metal door blocks the heat from escaping.\n");
     if (figureExamined == 0)
     {
         printf("You're honestly not sure what to think of the figure in the corner, but you resolve to not think about it.\n");
@@ -59,10 +74,21 @@ void theTunnels()
     }
     else
     {
-        printf("You've never been more terrified in your life, and you should certainly contact the police, but hey, you're alive!\n");
-        printf("You've managed to make it through the tunnels, albiet slightly traumatized.\n");
+        if (playerInjured == 1)
+        {
+            printf("You definitely need to go to a hospital, but at least you're not dead?\n");
+            printf("You're never going into those tunnels again.");
+        }
+        else
+        {
+            printf("You've never been more terrified in your life, and you should certainly contact the police, but hey, you're alive!\n");   
+            printf("You've managed to make it through the tunnels, albiet slightly traumatized.\n");
+        }
     }
+    
 }
+
+//Main Function!
 
 int main (void)
 {
